529b7578f6905befcc604df8530351db0cecd024
Christopher B. Grullon <31022904+cbgrullon@users.noreply.github.com>
2019-03-19 19:05:47 -0400

    Initial Commit

---
+++ ConsoleApp1/CallApi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    public class CallApi<T> : IDisposable where T : IEntity
+    {
+        HttpClient client = new HttpClient();
+        private string Url;
+        public CallApi(string Url)
+        {
+            client.BaseAddress = new Uri(Url);
+            this.Url = Url;
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<List<T>> GetAll()
+        {
+            List<T> list = new List<T>();
+            using (HttpResponseMessage response = await client.GetAsync("todos"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    list = await response.Content.ReadAsAsync<List<T>>();
+                }
+                return list;
+            }
+        }
+        public async Task<T> Post(T model)
+        {
+            using (HttpResponseMessage response = await client.PostAsJsonAsync("todos", model))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsAsync<T>();
+            }
+        }
+        public async Task<T> Put(T model)
+        {
+            using (HttpResponseMessage response = await client.PutAsJsonAsync($"todos/{model.id}", model))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsAsync<T>();
+            }
+        }
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var api = new CallApi<Todo>("https://jsonplaceholder.typicode.com/"))
+            {
+                var b = api.Put(new Todo { userId=1,id=1,title="Testing",completed=true});
+                Console.WriteLine("Put");
+                Console.WriteLine(JsonConvert.SerializeObject(b));
+                Console.WriteLine("Post");
+                var a = api.Post(new Todo { title = "Hey", userId = 1, completed = false }).GetAwaiter().GetResult();
+                Console.WriteLine(JsonConvert.SerializeObject(a));
+                Console.WriteLine("Get");
+                var list = api.GetAll().GetAwaiter().GetResult();
+                Console.WriteLine(JsonConvert.SerializeObject(list));
+                Console.ReadLine();
+            }            
+        }
+    }
+}
